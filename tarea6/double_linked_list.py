f3cf63c90cb92f10b1b4332c17c4cd29a61ddc60
Dante Faña Badia <dfana@dfb.com.do>
2021-02-10 01:12:06 -0400

    Add comment tarea6

--- tarea6/double_linked_list.py
+++ tarea6/double_linked_list.py
@@ -24,6 +24,7 @@ class DoubleLinkedList:
         self.current = self.nil.next
         self.size = 0
 
+    #** Para agregar vemos que la complejidad es O(1) siendo el tiempo constantes.
     def add(self, node: Node):
         node.next = self.nil.next
         self.nil.next.prev = node
@@ -31,6 +32,7 @@ class DoubleLinkedList:
         node.prev = self.nil
         self.size += 1
 
+    #** Dado que tenemos que recorrer un arreglo N para agregarlo en la lista la complejidad de O(n)
     def add_index(self, index: int, node: Node):
         assert index <= self.size, "Index out of bounds"
         node0 = self.get(index - 1) if index > 1 else self.nil
@@ -40,24 +42,29 @@ class DoubleLinkedList:
         node0.prev = node
         self.size += 1
 
+    #** Para add all cuando tenemos que recorrer la lista esta cambia a O(n) pues es lineal y el tiempo depende de N
     def add_all(self, *nodes):
         for node in nodes:
             self.add(node)
 
+    #** Para limpiar vemos que la complejidad es O (1) siendo el tiempo constantes.
     def clear(self):
         self.nil.next = self.nil
         self.nil.prev = self.nil
         self.size = 0
 
+    #** Para clonar vemos que la complejidad es O(N) siendo el tiempo lineal dado por el tamaño del N
     def clone(self):
         copy = type(self)()
         for node in self:
             copy.add(type(node)(node.data))
         return copy
 
+    #** Cuando estamos buscando en el medio o el final esta cambia a O(n)
     def __contains__(self, node: Node):
         return self.index(node)
 
+    #** Cuando tenemos que recorrer la lista en el medio o en el final cambia a O(n)
     def get(self, index: int):
         for index0, node in enumerate(self):
             if (index - 1) == index0:
@@ -68,9 +75,11 @@ class DoubleLinkedList:
         node.next.prev = node.prev
         self.size -= 1
 
+    #** Dado que tenemos que recorrer un arreglo buscando que nodo remover la complejidad es O(n)
     def remove(self, index: int):
         self._remove(self.get(index))
 
+    #** Para index cuando tenemos que recorrer la lista esta cambia a O(n) pues es lineal y el tiempo depende de N
     def index(self, node: Node):
         for index, node0 in enumerate(self):
             if node.data == node0.data:
