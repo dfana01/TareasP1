d607bfb8adbd5fcc1c7f00ff6233c26b59781b13
Dante Faña Badia <dfana@dfb.com.do>
2021-03-09 19:24:46 -0400

    Add tarea10

--- tarea10/main.py
+++ tarea10/main.py
@@ -1,31 +1,26 @@
-"""
+from datetime import datetime, timedelta
+from time import time
 
-"""
+class Mail:
+    epoch = 0
 
-class Student:
-    display_name = None
-    academic_index = 0
-    age = 0
-
-    def __init__(self, display_name, academic_index, age):
-        self.display_name = display_name
-        self.academic_index = academic_index
-        self.age = age
+    def __init__(self, epoch):
+        self.epoch = epoch
 
     def __gt__(self, other):
-        return self.age > other.age and self.academic_index > other.academic_index
+        return (self.epoch * -1) > (other.epoch * -1)
 
     def __lt__(self, other):
-        return self.age < other.age and self.academic_index < other.academic_index
+        return (self.epoch * -1) < (other.epoch * -1)
 
     def __eq__(self, other):
-        return self.age == other.age and self.academic_index == other.academic_index
+        return (self.epoch * -1) == (other.epoch * -1)
 
     def __str__(self):
-        return f"{self.display_name}, {self.age}, {self.academic_index}"
+        return f"{self.epoch}"
 
     def __repr__(self):
-        return f"{self.display_name}, {self.academic_index}, {self.age}"
+        return f"{self.epoch}"
 
 
 class HeapMax:
@@ -137,124 +132,27 @@ class HeapMax:
         return max_el
 
 
-class HeapMin:
+class MailerHandler:
     def __init__(self):
-        self.elements = []
-        self.size = 0
+        self.heap = HeapMax()
 
-    @staticmethod
-    def parent(i):
+    def queue(self, mail):
         """
         :complexity
-             :space: O(1), solo hace un calculo matematico
-             :time: O(1), solo hace un calculo matematico
+             :space: O(1), dado que solo ingresa el mail
+            :time: O(lg n), pues utiliza la complejidad de insert
         """
-        return i // 2
+        self.heap.insert(mail)
 
-    @staticmethod
-    def left(i):
+    def dequeue(self):
         """
         :complexity
-             :space: O(1), solo hace un calculo matematico
-             :time: O(1), solo hace un calculo matematico
+             :space: O(1), dado que no se usa variables
+             :time: O(lg n), ya que se usa extract max y se hereda la complejidad
         """
-        return 2 * i + 1
-
-    @staticmethod
-    def right(i):
-        """
-        :complexity
-             :space: O(1), solo hace un calculo matematico
-             :time: O(1), solo hace un calculo matematico
-        """
-        return 2 * i + 2
-
-    def insert(self, val):
-        """
-        :complexity
-             :space: O(1), dado que solo ingresa
-            :time: O(lg n), pues utiliza la complejidad de decrease_key
-        """
-        self.elements.append(val)
-        self.size = len(self.elements)
-        self.decrease_key(self.elements, self.size - 1)
-
-    def decrease_key(self, arr, i):
-        """
-        :complexity
-             :space: O(1), dado que solo usa variables especificas, eso no tomando en cuenta la llamada de stack
-             :time: O(lg n), pues recorre las raices de los subarboles
-        """
-        while arr[i] < arr[self.parent(i)]:
-            arr[i], arr[self.parent(i)] = arr[self.parent(i)], arr[i]
-            i = self.parent(i)
-
-    def heapify(self, arr, n, i):
-        """
-        :complexity
-             :space: O(1), dado que solo usa variables especificas, eso no tomando en cuenta la llamada de stack
-             :time: O(lg n), pues recorre el arbol recursivamente
-        """
-        largest = i
-        left = self.left(i)
-        right = self.right(i)
-
-        if left < n and arr[left] < arr[largest]:
-            largest = left
-
-        if right < n and arr[right] < arr[largest]:
-            largest = right
-
-        if largest != i:
-            arr[i], arr[largest] = arr[largest], arr[i]
-            self.heapify(arr, n, largest)
-
-    def build(self, elements):
-        """
-        :complexity
-             :space: O(1), dado que solo usa la misma lista
-             :time: O(n), pues recorre todos los elementos
-        """
-        self.elements = elements
-        self.size = len(elements)
-        for i in range(self.size//2 - 1, -1, -1):
-            self.heapify(self.elements, self.size, i)
-
-    def minimum(self):
-        """
-        :complexity
-             :space: O(1), dado que solo ingresa
-             :time: O(1), dado que solo se saca el primer elemento del arreglo
-        """
-        if len(self.elements) >= 1:
-            return self.elements[0]
-        else:
-            return None
-
-    def extract_min(self):
-        """
-        :complexity
-             :space: O(1), dado que solo usa variables
-             :time: O(lg n), ya que usa heapify
-        """
-        if len(self.elements) < 1:
-            raise Exception("Heap underflow")
-        max_el = self.minimum()
-        self.elements[0] = self.elements.pop()
-        self.size = len(self.elements)
-        self.heapify(self.elements, self.size, 0)
-        return max_el
+        return self.heap.extract_max()
 
 
-def heap_sort(head_class, elements):
-    """
-    :complexity
-         :space: O(1), dado que no es usa otra lista.
-         :time: O(n lg n), pues build element es de O(n) y el heapify toma O(lg n)
-    """
-    heap = head_class()
-    heap.build(elements)
-    for i in range(len(elements) - 1, 0, -1):
-        heap.elements[0], heap.elements[i] = heap.elements[i], heap.elements[0]
-        heap.heapify(elements, i, 0)
-    return heap.elements
+def get_epoch(days=1):
+    date = datetime.fromtimestamp(int(time())) + timedelta(days=days)
+    return int(date.timestamp())
--- tarea10/test.py
+++ tarea10/test.py
@@ -1,92 +1,19 @@
 import unittest
-from main import HeapMax, HeapMin, heap_sort, Student
+from main import Mail, MailerHandler, get_epoch
 
-STUDENTS = [
-    Student("Aamina", 4.0, 19),
-    Student("Ayyub", 3.7, 17),
-    Student("Eben", 2.3, 21),
-    Student("Elis", 3.2, 28),
-    Student("Zeta", 1.4, 29),
-    Student("Hashim", 3.6, 23),
-    Student("Roshni", 2.1, 36),
-    Student("Simona", 3.4, 33),
-    Student("Tanisha", 1.6, 23),
-    Student("Taslima", 3.1, 18),
-    Student("Ty", 3.0, 20)
-]
-
-STUDENTS_ORDERS_MIN = [
-    Student("Aamina", 4.0, 19),
-    Student("Simona", 3.4, 33),
-    Student("Tanisha",  1.6, 23),
-    Student("Zeta",  1.4, 29),
-    Student("Hashim", 3.6, 23),
-    Student("Roshni", 2.1, 36),
-    Student("Elis", 3.2, 28),
-    Student("Eben", 2.3, 21),
-    Student("Taslima", 3.1, 18),
-    Student("Ty", 3.0, 20),
-    Student("Ayyub", 3.7, 17)
-]
-
-STUDENTS_ORDERS_MAX = [
-    Student("Ayyub", 3.7, 17),
-    Student("Ty", 3.0, 20),
-    Student("Simona", 3.4, 33),
-    Student("Zeta", 1.4, 29),
-    Student("Eben", 2.3, 21),
-    Student("Roshni", 2.1, 36),
-    Student("Elis", 3.2, 28),
-    Student("Tanisha", 1.6, 23),
-    Student("Taslima", 3.1, 18),
-    Student("Hashim", 3.6, 23),
-    Student("Aamina", 4.0, 19),
-]
+MAX_PRIORITY_MAIL = Mail(get_epoch(days=1))
 
 
-class SearchTest(unittest.TestCase):
-    def test_heap_max_insert(self):
-        heap = HeapMax()
-        heap.insert(Student("Aamina", 4.0, 19))
-        heap.insert(Student("Ayyub", 3.7, 17))
-        heap.insert(Student("Eben", 2.3, 21))
-        heap.insert(Student("Elis", 3.2, 28))
-        heap.insert(Student("Zeta", 1.4, 29))
-        heap.insert(Student("Hashim", 3.6, 23))
-        heap.insert(Student("Roshni", 2.1, 36))
-        heap.insert(Student("Simona", 3.4, 33))
-        heap.insert(Student("Tanisha", 1.6, 23))
-        heap.insert(Student("Taslima", 3.1, 18))
-        heap.insert(Student("Ty", 3.0, 20))
-        self.assertEqual(heap.extract_max(), Student("Aamina", 4.0, 19))
-
-    def test_heap_min_insert(self):
-        heap = HeapMin()
-        heap.insert(Student("Aamina", 4.0, 19))
-        heap.insert(Student("Ayyub", 3.7, 17))
-        heap.insert(Student("Eben", 2.3, 21))
-        heap.insert(Student("Elis", 3.2, 28))
-        heap.insert(Student("Zeta", 1.4, 29))
-        heap.insert(Student("Hashim", 3.6, 23))
-        heap.insert(Student("Roshni", 2.1, 36))
-        heap.insert(Student("Simona", 3.4, 33))
-        heap.insert(Student("Tanisha", 1.6, 23))
-        heap.insert(Student("Taslima", 3.1, 18))
-        heap.insert(Student("Ty", 3.0, 20))
-        self.assertEqual(heap.extract_min(), Student("Ayyub", 3.7, 17))
-
-    def test_heap_build(self):
-        heap = HeapMax()
-        heap.build([17, 2, 1, 7, 3, 19, 36, 25, 100])
-        self.assertEqual(heap.extract_max(), 100)
-
-    def test_heapsort_max(self):
-        r = heap_sort(HeapMax, STUDENTS.copy())
-        self.assertEqual(r, STUDENTS_ORDERS_MAX)
-
-    def test_heapsort_min(self):
-        r = heap_sort(HeapMin, STUDENTS.copy())
-        self.assertEqual(r, STUDENTS_ORDERS_MIN)
+class MailerHandlerTest(unittest.TestCase):
+    def test_deque(self):
+        handler = MailerHandler()
+        handler.queue(MAX_PRIORITY_MAIL)
+        handler.queue(Mail(get_epoch(days=5)))
+        handler.queue(Mail(get_epoch(days=4)))
+        handler.queue(Mail(get_epoch(days=6)))
+        handler.queue(Mail(get_epoch(days=4)))
+        handler.queue(Mail(get_epoch(days=5)))
+        self.assertEqual(handler.dequeue(), MAX_PRIORITY_MAIL)
 
 
 if __name__ == '__main__':
