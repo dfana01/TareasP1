690d0f1cfa8da67b5fb17d7efa9dcb1092893135
Dante Faña Badia <dfana@dfb.com.do>
2021-03-13 13:07:47 -0400

    WIP: tarea11

---
+++ tarea11/test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import *
+
+
+class DirectAccessTableTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
