85653e9f60a88987fffe53a68ae03ba1a123fbf3
Dante Faña Badia <dfana@dfb.com.do>
2021-03-22 19:51:51 -0400

    indent staircase

--- hackerranck-unit-test/python/test/test/staircase_test.py
+++ hackerranck-unit-test/python/test/test/staircase_test.py
@@ -4,6 +4,7 @@ from problems.staircase import staircase
 
 OUTPUT = "     #\n    ##\n   ###\n  ####\n #####\n######\n"
 
+
 class Test(unittest.TestCase):
     def test_case_1(self):
         self.assertEqual(staircase(6), OUTPUT)
